0203c99e2c2ed8ab5c56f2afc9aeb5288c9afef4
James Hudson <james.hudson@pod-point.com>
2020-09-09 13:39:39 +0100

    SWP-1918 - New test

---
+++ tests/Providers/ReviewsCoUK/InviteOptionsTest.php
@@ -0,0 +1,40 @@
+<?php
+
+
+namespace PodPoint\Reviews\Tests\Providers\ReviewsCoUK;
+
+use PodPoint\Reviews\Providers\ReviewsCoUk\InviteOptions;
+use PodPoint\Reviews\Tests\TestCase;
+
+class InviteOptionsTest extends TestCase
+{
+    /**
+     * Setup
+     */
+    public function setUp()
+    {
+        parent::setUp();
+    }
+
+    /**
+     * Tests that when you create a new InviteOptions object from the InviteOptions class the values are correctly
+     * mapped from the array passed to the constructor to the relevant object's variables.
+     *
+     */
+    public function testNewlyCreatedInviteOptionsMapsValuesCorrectly()
+    {
+        $name = $this->faker->name;
+        $email = $this->faker->email;
+        $orderNumber = $this->faker->uuid;
+
+        $getOptions = new InviteOptions([
+            'name' => $name,
+            'email' => $email,
+            'orderNumber' => $orderNumber,
+        ]);
+
+        $this->assertEquals($getOptions->name, $name);
+        $this->assertEquals($getOptions->email, $email);
+        $this->assertEquals($getOptions->orderNumber, $orderNumber);
+    }
+}
