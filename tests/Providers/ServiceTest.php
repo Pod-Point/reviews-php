b14ab449b5162f64c81411cfaeda6e65a575dbf5
James Hudson <james.hudson@pod-point.com>
2020-09-09 12:53:48 +0100

    SWP-1918 - Seeing what Travis thinks of my new test.

--- tests/Providers/ServiceTest.php
+++ tests/Providers/ServiceTest.php
@@ -3,8 +3,53 @@
 
 namespace PodPoint\Reviews\Tests;
 
+use \GuzzleHttp\Client as GuzzleClient;
+use PodPoint\Reviews\Providers\ReviewsCoUk\Configuration;
+use PodPoint\Reviews\Providers\ReviewsCoUk\Service;
 
-class ServiceTest
+class ServiceTest extends TestCase
 {
+    const URL = 'localhost';
+    const STORE = 'someStore';
+    const API_KEY = 'someApiKey';
 
+    /**
+     * @var \PHPUnit\Framework\MockObject\MockObject
+     */
+    private $mockClient;
+
+    /**
+     * @var Service
+     */
+    private $client;
+
+    /**
+     * Mocks the config & guzzle client and creates a client instance.
+     */
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->mockClient = $this->createMock(GuzzleClient::class);
+
+        $this->client = new Service($this->mockClient, new Configuration([
+            'url' => self::URL,
+            'store' => self::STORE,
+            'apiKey' => self::API_KEY,
+        ]));
+    }
+
+    /**
+     * Ensure the Service can send invites.
+     */
+    public function testCanSendInviteToReview()
+    {
+        $options = [
+            'name',
+            'email',
+            'orderNumber'
+        ];
+
+        $this->client->invite();
+    }
 }
