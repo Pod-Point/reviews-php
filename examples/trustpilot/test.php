ec62b9d53a149b071721ea450c8011a0e51abd83
Andreia Silva <andreia.matias.silva@gmail.com>
2020-10-01 08:52:03 +0100

    Update example

--- examples/trustpilot/test.php
+++ examples/trustpilot/test.php
@@ -2,17 +2,10 @@
 
 require __DIR__ . '/../../vendor/autoload.php';
 
-
 try {
-    $manager = new \PodPoint\Reviews\Manager($httpClient);
+    $manager = new \PodPoint\Reviews\Manager([]);
     $reviewService = $manager->withProvider('trustpilot');
-
-    $reviewService->product();
-
-    $reviewService->service();
-
     $invite = $reviewService->service()->invite($options);
-
 
 } catch (Exception $exception) {
     var_dump($exception);
