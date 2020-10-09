d13d32f18361681fbf328f5f731a76fccd254926
Ersin D <ersin.demirtas@pod-point.com>
2020-10-09 14:13:04 +0100

    wip

--- src/config/review-providers.php
+++ src/config/review-providers.php
@@ -10,7 +10,6 @@ return [
     'reviews_io' => [
         'store' => env('REVIEWS_CO_UK_STORE'),
         'api_key' => env('REVIEWS_CO_UK_API_KEY'),
-        'api_base_host' => env('REVIEWS_CO_UK_API_BASE_HOST'),
     ],
     
     'trustpilot' => [
@@ -19,7 +18,5 @@ return [
         'client_secret' => env('TRUSTPILOT_SECRET_KEY'),
         'client_id' => env('TRUSTPILOT_CLIENT_ID'),
         'business_unit_id' => env('TRUSTPILOT_BUSINESS_ID'),
-        'api_base_host' => env('TRUSTPILOT_API_BASE_HOST'),
-        'api_invitation_host' => env('TRUSTPILOT_API_INVITATION_HOST'),
     ],
 ];
