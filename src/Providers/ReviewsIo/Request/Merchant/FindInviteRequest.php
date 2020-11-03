74def3267329bdb54f13570ccc06df2e104919af
Martin Stuessy <martin.stuessy@pod-point.com>
2020-11-03 16:01:39 +0000

    Introduce ReviewsIo FindInviteRequest.

---
+++ src/Providers/ReviewsIo/Request/Merchant/FindInviteRequest.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace PodPoint\Reviews\Providers\ReviewsIo\Request\Merchant;
+
+use GuzzleHttp\Psr7\Request;
+use PodPoint\Reviews\Request\BaseRequestWrapper;
+
+/**
+ * Class FindInviteRequest.
+ */
+class FindInviteRequest extends BaseRequestWrapper
+{
+    /**
+     * Builds the request.
+     *
+     * @return Request
+     */
+    public function getRequest(): Request
+    {
+        $query = http_build_query([
+            'store' => $this->getOption('store'),
+            'order_number' => $this->getOption('reviewId'),
+        ]);
+
+        return new Request('GET', '/merchant/invitation?' . $query);
+    }
+
+    /**
+     * List of required fields.
+     *
+     * @return array
+     */
+    public function requiredFields(): array
+    {
+        return [
+            'order_number',
+        ];
+    }
+
+    /**
+     * Sends the request and parses response into array.
+     *
+     * @return array|mixed
+     */
+    public function send()
+    {
+        $response = $this->httpClient->sendRequest(
+            $this->getRequest(),
+            true
+        );
+
+        return $this->httpClient->getResponseJson($response);
+    }
+}
